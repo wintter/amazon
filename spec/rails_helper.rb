a023848b67d9f0c4537ddda23bc150c0c5c043a8
wintter <chesterfield252@gmail.com>
2015-06-25 12:07:40 +0300

    update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -48,4 +49,25 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    # This says that before the entire test suite runs, clear the test database out completely.
+    # This gets rid of any garbage left over from interrupted or poorly-written tests - a common source of surprising test behavior.
+    DatabaseCleaner.clean_with(:truncation)
+
+    # This part sets the default database cleaning strategy to be transactions.
+    # Transactions are very fast, and for all the tests where they do work - that is, any test where the entire test runs in the RSpec process - they are preferable.
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # These lines hook up database_cleaner around the beginning and end of each test,
+  # telling it to execute whatever cleanup strategy we selected beforehand.
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
